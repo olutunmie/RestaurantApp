a222e59f234431c8b73492f6dd7315f1fe5d50fd
tunmie <victoroluwasomidotun@gmail.com>
2023-01-25 03:53:35 +0100

    migrated to sdk 33

--- app/src/main/java/dev/tunmie/restaurantapp/RestaurantDetailsViewModel.kt
+++ app/src/main/java/dev/tunmie/restaurantapp/RestaurantDetailsViewModel.kt
@@ -1,13 +1,17 @@
 package dev.tunmie.restaurantapp
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RestaurantDetailsViewModel(): ViewModel() {
 	private var restInterface: RestaurantsApiService
+	val state = mutableStateOf<Restaurant?>(null)
 	init {
 		val retrofit: Retrofit = Retrofit.Builder()
 			.addConverterFactory(GsonConverterFactory.create())
@@ -16,6 +20,10 @@ class RestaurantDetailsViewModel(): ViewModel() {
 		restInterface = retrofit.create(
 			RestaurantsApiService::class.java
 		)
+		viewModelScope.launch {
+			val restaurant = getRemoteRestaurant(2)
+			state.value = restaurant
+		}
 	}
 
 	private suspend fun getRemoteRestaurant(id: Int) : Restaurant {
